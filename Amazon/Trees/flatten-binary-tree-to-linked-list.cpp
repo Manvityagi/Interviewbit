89118a5b5a73558d37a5ac466770abe926c07574
Manvi Tyagi <manvityagi770@gmail.com>
2020-06-16 18:14:38 +0530

    started trees

--- Amazon/Trees/flatten-binary-tree-to-linked-list.cpp
+++ Amazon/Trees/flatten-binary-tree-to-linked-list.cpp
@@ -4,14 +4,16 @@
 If at each step, we maintain an updated parent,  we just need to do recursive calls
 
 (2) ITERATIVE SOLUTION
+    node X ke left me jo rakha hai, usey itha ke node ke right me rakhna hai, 
+    aur jo left se subtree uthaya tha, uske rightmost me node X ka orignal right daalna hai, 
+    ye hi process har right node ke liye karna hai, 
+
+
 
 
 */
 
-
-
 // --------------------------------RECURSIVE SOLUTION------------------------------------------//
-
 
 TreeNode *parent = nullptr;
 void dfs(TreeNode *root)
@@ -35,7 +37,6 @@ TreeNode *Solution::flatten(TreeNode *A)
     return A;
 }
 
-
 // --------------------------------ITERATIVE SOLUTION------------------------------------------//
 
 TreeNode *Solution::flatten(TreeNode *root)
@@ -47,13 +48,14 @@ TreeNode *Solution::flatten(TreeNode *root)
         if (iter)
         {
             while (iter->right)
-            {
                 iter = iter->right;
-            }
+
             iter->right = now->right;
             now->right = now->left;
             now->left = NULL;
         }
         now = now->right;
     }
+
+    return root;
 }
---
+++ Amazon/Trees/identical-binary-trees.cpp
@@ -0,0 +1,20 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+int Solution::isSameTree(TreeNode *A, TreeNode *B)
+{
+
+    if ((A && !B) || (B && !A))
+        return 0;
+
+    if (!A && !B)
+        return 1;
+
+    return (A->val == B->val && isSameTree(A->left, B->left) && isSameTree(A->right, B->right));
+}
